1158fd25e4761f1a89d95bdd558422f5871909a1
Aditya Sreekumar <aditya.sreekumar@gmail.com>
2026-03-31 22:11:32 -0400

    chore(ui): remove absolute dev ips from nextjs configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Allow hot module reloading when testing over the LAN network ip
-  allowedDevOrigins: ['192.168.1.176'],
+  /* config options here */
 };
 
 export default nextConfig;
